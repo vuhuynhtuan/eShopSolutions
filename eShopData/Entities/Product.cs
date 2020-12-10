c929bdad9ce854d4f2f0587463fb6655ca8fa9f5
vuhuynhtuan <vuhuynhtuan@gmail.com>
2020-12-10 16:33:04 +0900

    thiet ke database

---
+++ eShopData/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopData.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        //Giá bán
+        public decimal Price { get; set; }
+        //giá gốc
+        public decimal OriginalPrice { get; set; }
+        // số lượng tồn trong kho 
+        public int Stock { get; set; }
+        public int ViewCount { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string SeoAlias { get; set; }
+        public List<ProductInCategory> ProductInCategories { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+    }
+}
---
+++ eShopData/Enums/Status.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopData.Enums
+{
+    public enum Status
+    {
+        InActive,
+        Active
+    }
+}
